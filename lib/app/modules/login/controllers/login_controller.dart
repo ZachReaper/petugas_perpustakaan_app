705e1ed9e746146d04a2eae1b622df56acb4f7e3
rayhanwkwk <rayhanristyawan@gmail.com>
2024-01-17 13:54:21 +0700

    home pages

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart' as dio;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +24,11 @@ class LoginController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+    String status = StorageProvider.read(StorageKey.status);
+    log("status : $status");
+    if(status == "Logged"){
+      Get.offAllNamed(Routes.HOME);
+    }
   }
 
   @override
